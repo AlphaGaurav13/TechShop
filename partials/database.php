9f8ddf1f493855e41e7d98f822d49e6f3c0cd161
Gaurav132005 <gauravkkr345@gmail.com>
2025-12-18 00:58:54 +0530

    Change in db connections

--- partials/database.php
+++ partials/database.php
@@ -3,7 +3,7 @@ $host="localhost";
 $user="root";
 $password="";
 $database="e-commerce";
-$conn=mysqli_connect($host,$user,$password,$database);
+$conn=mysqli_connect($host,$user,$password,$database,3307);
 if(!$conn){
    
     die("Error".mysqli_connect_error());
