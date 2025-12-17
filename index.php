b44306ddb6af0f367de9ac2e31393b17b6c6b0e9
Gauravkumar <144926875+AlphaGaurav13@users.noreply.github.com>
2025-12-18 01:23:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>TechShop</title>
+  <title>Gauravuff</title>
   <?php include './partials/commonfiles.php'; ?>
 </head>
 
