2c61053c86bd61bd4f3d547c05493fb257d259fd
Gaurav132005 <gauravkkr345@gmail.com>
2025-12-27 13:22:09 +0530

    done

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Aaurav</title>
+  <title>Gaurav</title>
   <?php include './partials/commonfiles.php'; ?>
 </head>
 
